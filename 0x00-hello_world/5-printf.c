0a0846d87079580d4ce9d2f87bfe203ec5234a75
Salmasraidi <salmaseraidi@gmail.com>
2023-08-10 00:02:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stadio.h>
+/**
+ * main - A program that print a line using the printf fonction
+ * Return: 0 (Success)
+ */
+
+int mant(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n")
+    return (0);
+}
+    
