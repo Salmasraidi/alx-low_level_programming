f996a1192c008df9352058c145a377467b5b92ce
Salmasraidi <salmaseraidi@gmail.com>
2023-08-13 19:16:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of tow digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n, m;
+
+for (n = 48; n <= 56; n++)
+{
+for (n = 49; m <= 57; m++);
+{
+if (m > n)
+{
+putchar(n);
+putchar(m)
+if (n != 58 || m != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
