64599df70a69d1db9725939673219d676c4f4fd0
Salmasraidi <salmaseraidi@gmail.com>
2023-08-15 14:21:42 -0700

    first part

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,16 +21,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endifvoid print_alphabet_x10(void);
-int _islower(int c);
-int _islower(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
 #endif
