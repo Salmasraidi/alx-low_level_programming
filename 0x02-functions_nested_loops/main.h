60dd79cd018433361bc802f921e36fb13fd8addd
Salmasraidi <salmaseraidi@gmail.com>
2023-08-15 14:31:04 -0700

    #ifndef main_h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 /**
  * Desc: Header file containing declarations for all functions
- * used in the 0x02-functions_nested_loops directory.
+ *       used in the 0x02-functions_nested_loops directory.
  *
  */
 
